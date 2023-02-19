50a580084b5a09e0b62fe068cda32744463ac885
zerodott <93039953+casperDev3@users.noreply.github.com>
2023-02-19 12:18:07 +0200

    test

--- src/js/script.js
+++ src/js/script.js
@@ -1,103 +1,83 @@
-let POSTS = [
+let personalInfo = [
   {
-    id: 1,
-    title: 'Post 1',
-    body: 'Post 1 body',
-    price: 300,
-    comments: [
-      'Comment 1', 'Comment 2', 'Comment 3'
-    ]
+    id: '1',
+    first_name: 'John',
+    last_name: 'Doe',
+    fathers_name: 'Hohn',
+    comments: ['test', 'test2']
   },
   {
-    id: 2,
-    title: 'Post 2',
-    body: 'Post 2 body',
-    price: 400,
-    comments: [
-      'Comment 1', 'Comment 2', 'Comment 3'
-    ]
+    id: '2',
+    first_name: 'Jane',
+    last_name: 'Doe',
+    fathers_name: 'Hohn',
+    comments: []
   },
   {
-    id: 3,
-    title: 'Post 3',
-    body: 'Post 3 body',
-    price: 500,
-    comments: [
-      'Comment 1', 'Comment 2', 'Comment 3'
-    ]
+    id: '3',
+    first_name: 'Peter',
+    last_name: 'Kowalski',
+    fathers_name: 'Piiiddi',
+    comments: ['drr']
   }
 ]
 
-function showPostsOnPage(posts) {
+
+function showPostsOnPage(posts, id) {
   // 1. Forming the HTML for the post
   let html = '';
   posts.forEach((post) => {
+    let commentsCode = '';
+    post.comments.forEach((comment) => {
+      commentsCode += `
+        <p>${comment}</p> 
+      `
+    })
+    const { id, first_name, last_name, fathers_name, comments } = post;
     html += `
-    <div id="${post.id}" class="item">
-      <h2 class="item__title">${post.title}</h2>
-      <p class="item__price">${post.price}</p>
-      <p class="item__text">${post.body}</p>
-    </div>`
+    <div id="${id}" class="person__card">
+      <h1 class="person__card_initials">${last_name} ${first_name[0]}. ${fathers_name[0]}.</h1>
+      <p class="person__card_comments">${comments}</p>
+      <button class="add_comments_btn">Add Comment</button>
+    </div>
+    `
   })
   // 2. Inserting the HTML into the DOM
-  const TARGET = document.querySelector('#posts_list');
+  const TARGET = document.getElementById(id);
   TARGET.innerHTML = html;
 }
-function addPost(e) {
-  e.preventDefault();
-  // 1. Getting the data from the form
-  const TITLE = document.getElementById('title').value
-  const BODY = document.getElementById('description').value
-  const PRICE = document.getElementById('price').value
-  // 2. Forming the post object
-  const POST = {
-    id: POSTS.length + 1,
-    title: TITLE,
-    body: BODY,
-    price: PRICE,
-    comments: []
-  }
-  // 3. Adding the post to the array
-  POSTS.push(POST)
-  // 4. Showing the posts on the page
-  showPostsOnPage()
-  // 5. Clearing the form
-  const FORM = document.querySelector('form');
-  FORM.reset();
+
+
+function changeTextToCapitalize(text) {
+  return text[0].toUpperCase() + text.slice(1).toLowerCase();
 }
 
-function showTextArea() {
-  // 1. Getting checkbox checked status
-  const CHECKBOX = document.getElementById('show_textarea');
-  const CHECKED = CHECKBOX.checked;
-  const TEXTAREA = document.getElementById('content');
-  console.log(CHECKED);
-
-  // if(CHECKED){
-  //   TEXTAREA.classList.add('show');
-  // }else{
-  //   TEXTAREA.classList.remove('show');
-  // }
-  CHECKED ? TEXTAREA.classList.add('show') : TEXTAREA.classList.remove('show');
+function showPersonalInfo(content, id) {
+  if (id === 'first_name' || id === 'fathers_name') {
+    content = content[0].toUpperCase() + ".";
+  }
+  const ELEMENT = document.getElementById(`output_${id}`);
+  ELEMENT.innerHTML = changeTextToCapitalize(content);
 }
 
-function filterPostsByPrice(e) {
-  e.preventDefault();
-  function filter() {
-    const FILTERED_POSTS = POSTS.filter((post) => {
-      return post.price >= MIN_PRICE;
-    });
-    showPostsOnPage(FILTERED_POSTS);
-  }
-  function displayNotificationUser() {
-    alert('Please input price!');
-  }
-  const MIN_PRICE = document.getElementById('filter_price').value;
-  MIN_PRICE.length == 0 || Number(MIN_PRICE) <= 0 ? displayNotificationUser() : filter();
+// start of the script
+function getPersonalInfo(e) {
+  const VALUE = e.target.value;
+  const ID_ELEMENT = e.target.id;
+  showPersonalInfo(VALUE, ID_ELEMENT);
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-  showPostsOnPage(POSTS);
-})
+  showPostsOnPage(personalInfo, 'posts');
 
+  const ADD_COMMENT_BTN = document.querySelectorAll('.add_comments_btn');
+  ADD_COMMENT_BTN.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+      alert('Add comment');
+      //1. Get the id of the post parent
+      const ID = e.target.parentElement.id;
+      console.log(ID);
+    })
+  });
+});
 
