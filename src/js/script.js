d2655f20f6363e4a0500251784b46e97af459e8d
zerodott <93039953+casperDev3@users.noreply.github.com>
2023-03-05 12:10:56 +0200

    add lection

--- src/js/script.js
+++ src/js/script.js
@@ -122,14 +122,115 @@ MOBILE_MENU_LINKS.forEach((link) => {
   })
 })
 
+
+
+
 // showelement on scroll
 document.addEventListener('scroll', () => {
   const ELEMENT = document.querySelector('.scroll-up');
   // display scroll up button after 100px
   if (window.scrollY > 20) {
     ELEMENT.classList.add('show');
-  }else{
+  } else {
     ELEMENT.classList.remove('show');
   }
-
 })
+
+const FRUITS = [
+  {
+    id: 1,
+    name: 'Apple',
+    price: 500,
+  },
+  {
+    id: 2,
+    name: 'Orange',
+    price: 200,
+  },
+  {
+    id: 3,
+    name: 'Banana',
+    price: 300,
+  }
+];
+const ELECTRONICS = [
+  {
+    id: 1,
+    name: 'Laptop',
+    price: 1000,
+  },
+  {
+    id: 2,
+    name: 'Phone',
+    price: 2000,
+  },
+  {
+    id: 3,
+    name: 'TV',
+    price: 3000,
+  }
+];
+const PRODUCTS = [...ELECTRONICS, ...FRUITS];
+
+const TABS = document.querySelectorAll('.tabs__item');
+const CONTENT = document.querySelectorAll('.tabs__content');
+
+function showProductsOnPage(selector, array) {
+  let html = '';
+  array.forEach((item) => {
+    html += `
+    <div class="item" id="${item.id}">
+      <h2 class="item__title">${item.name}</h2>
+      <p class="item__price">${item.price}</p>
+    </div>`
+  });
+  const TARGET = document.querySelector(selector);
+  TARGET.innerHTML = html;
+}
+
+
+TABS.forEach((tab) => {
+  tab.addEventListener('click', () => {
+    TABS.forEach((tab) => {
+      tab.classList.remove('selected');
+    });
+    tab.classList.add('selected');
+    const DATA_ATTRIBUTE = tab.getAttribute('data-products');
+    CONTENT.forEach((item) => {
+      item.classList.add('d-none');
+      if (item.getAttribute('data-products') == DATA_ATTRIBUTE) {
+        item.classList.remove('d-none');
+      }
+    });
+  })
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  showProductsOnPage('#fruits', FRUITS);
+  showProductsOnPage('#electronics', ELECTRONICS);
+  showProductsOnPage('#all_products', PRODUCTS);
+})
+
+const ABS_BTN = document.querySelector('#abs');
+ABS_BTN.addEventListener('click', () => {
+  const SELECTED_TAB = document.querySelector('.tabs__item.selected');
+  const DATA_ATTRIBUTE = SELECTED_TAB.getAttribute('data-id');
+  if (DATA_ATTRIBUTE == 'fruits') {
+    FRUITS.sort((a, b) => {
+      return a.price - b.price;
+    });
+    showProductsOnPage('#fruits', FRUITS);
+  } else if (DATA_ATTRIBUTE == 'electronics') {
+    ELECTRONICS.sort((a, b) => {
+      return a.price - b.price;
+    });
+    showProductsOnPage('#electronics', ELECTRONICS);
+  }else{
+    PRODUCTS.sort((a, b) => {
+      return a.price - b.price;
+    });
+    showProductsOnPage('#all_products', PRODUCTS);
+  }
+});
+
+
